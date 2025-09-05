496f31b45bf6e5f8d0c4620311cb123b77957f05
HarshAgarwal4 <agarwalh270@gmail.com>
2025-09-05 16:55:29 +0530

    proxy

--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -2,6 +2,6 @@ import axios from 'axios'
 const url = import.meta.env.VITE_REACT_APP_BACKEND_URL
 
 export default axios.create({
-    baseURL: "https://chat-backend-silk-nine.vercel.app",
+    baseURL: url,
     withCredentials: true
 })
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/": {
+        target: "http://localhost:5000", // backend
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
